50c718341ff13356caad94218787dde9b1552bd2
iamfelipes <flipe.silva@terra.com.br>
2024-04-05 11:20:57 -0300

    add:checkbox

--- src/app/main-app/pedido/criar-pedido/passo14/passo14.component.ts
+++ src/app/main-app/pedido/criar-pedido/passo14/passo14.component.ts
@@ -12,7 +12,7 @@ export class Passo14Component {
     constructor(
         public pedidoService: PedidoService,
         private _toaster: ToasterService
-    ) {}
+    ) { }
 
     ngOnInit() {
         this.pedidoService.getObservable().subscribe(() => {
@@ -30,6 +30,7 @@ export class Passo14Component {
     selectedVidro: string = '';
     selectedPorta: string = '';
     selected: string = '';
+    selectedOption: string = '';
 
     optionsVidro: any[] = [
         {
@@ -80,6 +81,26 @@ export class Passo14Component {
         this.selectedPorta = option.name;
     }
 
+    toggleSelection(optionName: string): void {
+        this.selectedOption = optionName;
+        this.selected = optionName;
+
+        if (optionName.includes('PVC')) {
+            this.pedidoService.pedido.balcony.lock.pvc = true;
+            this.pedidoService.pedido.balcony.lock.ferro = false; 
+        } else if(optionName.includes('FERRO')) {
+                this.pedidoService.pedido.balcony.lock.ferro == true;
+                this.pedidoService.pedido.balcony.lock.pvc == false;
+            }
+
+        if (optionName.includes('1520/1531')) {
+            this.pedidoService.pedido.balcony.lock['1520/1531'] == true;
+            this.pedidoService.pedido.balcony.lock['3210/3211'] == false;
+        } else if (optionName.includes('3210/3211')) {
+            this.pedidoService.pedido.balcony.lock['3210/3211'] == true;
+            this.pedidoService.pedido.balcony.lock['1520/1531'] == false;
+        }
+    }
     nextTab(): void {
         const option = this.pedidoService.pedido.balcony.lock;
         if (option.fechadura_para_porta || option.fechadura_vidro_vidro) {

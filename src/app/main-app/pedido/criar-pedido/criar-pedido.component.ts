815273a3728cd2845609df1b09efdd96f6c47860
Kaio Moreira <kaio-dev@outlook.com>
2024-05-15 11:09:11 -0300

    add: message for notification user peido is save in draft

--- src/app/main-app/pedido/criar-pedido/criar-pedido.component.ts
+++ src/app/main-app/pedido/criar-pedido/criar-pedido.component.ts
@@ -26,31 +26,31 @@ export class CriarPedidoComponent implements CanComponentDeactivate, OnInit {
             localStorage.removeItem('pedido');
             this.pedidoService.setActiveIndex(0);
         }
-        if (this.pedidoString && !this.pedidoService.pedido.isDraft){
-            this.confirmationService.confirm({
-                message:
-                    'Encontramos um pedido em andamento, deseja dar prosseguimento?',
-                header: 'Confirmação',
-                icon: 'pi pi-exclamation-triangle',
-                acceptLabel: 'Sim',
-                rejectLabel: 'Não',
-                rejectButtonStyleClass: 'p-button-text',
-                accept: () => {
-                    this.pedidoService.pedido = JSON.parse(this.pedidoString);
-                    this.pedidoService.activeIndex =
-                    this.pedidoService.getActiveIndex();
-                    this.pedidoService.notifyObservers();
-                },
-                reject: () => {
-                    this.pedidoService.clearLocalStorage();
-                    this.pedidoService.activeIndex = 0;
-                    this.pedidoService.pedido = this.pedidoService.intilizePedido();
-                    this.pedidoService.setActiveIndex(0);
-                },
-            });
-        }else {
-            this.pedidoService.activeIndex = this.pedidoService.pedido?.activeIndex || 0;
-        }
+        // if (this.pedidoString && !this.pedidoService.pedido.isDraft){
+        //     this.confirmationService.confirm({
+        //         message:
+        //             'Encontramos um pedido em andamento, deseja dar prosseguimento?',
+        //         header: 'Confirmação',
+        //         icon: 'pi pi-exclamation-triangle',
+        //         acceptLabel: 'Sim',
+        //         rejectLabel: 'Não',
+        //         rejectButtonStyleClass: 'p-button-text',
+        //         accept: () => {
+        //             this.pedidoService.pedido = JSON.parse(this.pedidoString);
+        //             this.pedidoService.activeIndex =
+        //             this.pedidoService.getActiveIndex();
+        //             this.pedidoService.notifyObservers();
+        //         },
+        //         reject: () => {
+        //             this.pedidoService.clearLocalStorage();
+        //             this.pedidoService.activeIndex = 0;
+        //             this.pedidoService.pedido = this.pedidoService.intilizePedido();
+        //             this.pedidoService.setActiveIndex(0);
+        //         },
+        //     });
+        // }else {
+        //     this.pedidoService.activeIndex = this.pedidoService.pedido?.activeIndex || 0;
+        // }
 
         if(!this.pedidoString){
             const acessories = JSON.parse(localStorage.getItem('acessories'));
@@ -66,29 +66,33 @@ export class CriarPedidoComponent implements CanComponentDeactivate, OnInit {
 
     canDeactivate(): boolean | Promise<boolean> {
         if (this.pedidoService.activeIndex > 0) {
-            localStorage.removeItem('imagemBase64');
-            return new Promise<boolean>((resolve, reject) => {
-                this.confirmationService.confirm({
-                    message:
-                        'Deseja salvar este pedido como rascunho para completar mais tarde?',
-                    header: 'Confirmação',
-                    icon: 'pi pi-exclamation-triangle',
-                    acceptLabel: 'Sim',
-                    rejectLabel: 'Não',
-                    rejectButtonStyleClass: 'p-button-text',
-                    accept: () => {
-                        this.pedidoService.setActiveIndex(0);
-                        this.pedidoService.saveDraftPedido(
-                            this.pedidoService.pedido
-                        );
-                        resolve(true);
-                    },
-                    reject: () => {
-                        this.pedidoService.setActiveIndex(0);
-                        resolve(true);
-                    },
-                });
-            });
+            this.pedidoService.saveDraftPedido(
+                this.pedidoService.pedido
+            );
+            return true
+            // localStorage.removeItem('imagemBase64');
+            // return new Promise<boolean>((resolve, reject) => {
+            //     this.confirmationService.confirm({
+            //         message:
+            //             'Deseja salvar este pedido como rascunho para completar mais tarde?',
+            //         header: 'Confirmação',
+            //         icon: 'pi pi-exclamation-triangle',
+            //         acceptLabel: 'Sim',
+            //         rejectLabel: 'Não',
+            //         rejectButtonStyleClass: 'p-button-text',
+            //         accept: () => {
+            //             this.pedidoService.setActiveIndex(0);
+            //             this.pedidoService.saveDraftPedido(
+            //                 this.pedidoService.pedido
+            //             );
+            //             resolve(true);
+            //         },
+            //         reject: () => {
+            //             this.pedidoService.setActiveIndex(0);
+            //             resolve(true);
+            //         },
+            //     });
+            // });
         } else return true;
     }
 }

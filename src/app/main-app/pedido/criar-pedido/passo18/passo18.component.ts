cdf44fe1c6b045f4952c2a3141f5d29a6dc749d1
Kaio Moreira <kaio-dev@outlook.com>
2024-06-27 14:38:18 -0300

    fix: type values for send api

--- src/app/main-app/pedido/criar-pedido/passo18/passo18.component.ts
+++ src/app/main-app/pedido/criar-pedido/passo18/passo18.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { finalize } from 'rxjs';
 import { LoadingService } from 'src/app/components/loading/loading.service';
 import { ToasterService } from 'src/app/components/toaster/toaster.service';
-import { PedidoJson } from 'src/app/models/pedidoJson';
+import { Balcony, PedidoJson } from 'src/app/models/pedidoJson';
 import { ImageService } from 'src/app/services/image.service';
 import { PedidoService } from 'src/app/services/pedido.service';
 
@@ -35,10 +35,39 @@ export class Passo18Component {
     enviar() {
         const imagem = localStorage.getItem('imagemBase64');
         this.ldService.start();
-
+        this._toaster.info(imagem)
         if (imagem) {
             this.pedidoService
-                .create(this.pedidoService.pedido)
+                .create({
+                    ...this.pedidoService.pedido,
+                    balcony:{
+                        ...this.pedidoService.pedido.balcony,
+                        plumb: {
+                            left_wall: {
+                                top: String(this.pedidoService.pedido.balcony.plumb.left_wall.top),
+                                bottom: String(this.pedidoService.pedido.balcony.plumb.left_wall.bottom),
+                            },
+                            right_wall: {
+                                top: String(this.pedidoService.pedido.balcony.plumb.right_wall.top),
+                                bottom: String(this.pedidoService.pedido.balcony.plumb.right_wall.bottom),
+                            }
+                        },
+                        aperture: {
+                            ...this.pedidoService.pedido.balcony.aperture,
+                            locations: this.pedidoService.pedido.balcony.aperture.locations.map((aperture) => {
+                                return {
+                                    glasses: String(aperture.glasses),
+                                };
+                            })
+                        },
+                        levels: {
+                            measures: {
+                                // converter os valores de measures para string
+                                data: this.pedidoService.pedido.balcony.levels.measures.data.map((data) => data.map((value) => String(value))),
+                            }
+                        }
+                    } as unknown as Balcony
+                })
                 .pipe(finalize(() => (this.disableEnviar = true)))
                 .subscribe((response) => {
                     this.imageService
@@ -60,7 +89,36 @@ export class Passo18Component {
                 });
         } else {
             this.pedidoService
-                .create(this.pedidoService.pedido)
+                .create({
+                    ...this.pedidoService.pedido,
+
+                    balcony:{
+                        ...this.pedidoService.pedido.balcony,
+                        plumb: {
+                            left_wall: {
+                                top: String(this.pedidoService.pedido.balcony.plumb.left_wall.top),
+                                bottom: String(this.pedidoService.pedido.balcony.plumb.left_wall.bottom),
+                            },
+                            right_wall: {
+                                top: String(this.pedidoService.pedido.balcony.plumb.right_wall.top),
+                                bottom: String(this.pedidoService.pedido.balcony.plumb.right_wall.bottom),
+                            }
+                        },
+                        aperture: {
+                            ...this.pedidoService.pedido.balcony.aperture,
+                            locations: this.pedidoService.pedido.balcony.aperture.locations.map((aperture) => {
+                                return {
+                                    glasses: String(aperture.glasses),
+                                };
+                            })
+                        },
+                        levels: {
+                            measures: {
+                                data: this.pedidoService.pedido.balcony.levels.measures.data.map((data) => data.map((value) => String(value))),
+                            }
+                        }
+                    } as unknown as Balcony
+                })
                 .pipe(finalize(() => (this.disableEnviar = true)))
                 .subscribe((response) => {
                     this.pedidoService.dimensionOK = false;
--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,3 +1,59 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { environment } from 'src/environments/environment';
+
+// @Injectable({
+//     providedIn: 'root',
+// })
+// export class ImageService {
+//     apiUrl = environment.API_URL + '/images';
+
+//     constructor(private http: HttpClient) {}
+
+//     uploadImageUser(idUser: string, file: File): Observable<any> {
+//         const formData = new FormData();
+//         formData.append('image', new Blob([file]), file.name);
+//         return this.http.post<any>(this.apiUrl + '/user/' + idUser, formData);
+//     }
+
+//     uploadOrderImageFromLocalStorage(idOrder: string): Observable<any> {
+//         const formData = new FormData();
+//         const file = this.dataURItoBlob();
+//         formData.append('images', file, `image.${file.type.split('/')[1]}`);
+//         return this.http.post<any>(this.apiUrl + '/order/' + idOrder, formData);
+//     }
+
+//     storeFile(base64Image: string | null) {
+//         if (base64Image) {
+//           localStorage.setItem('imagemBase64', base64Image);
+//         }
+//       }
+
+//     dataURItoBlob() {
+//         const dataURI = localStorage.getItem('imagemBase64');
+//         if (dataURI) {
+//             const byteString = atob(dataURI.split(',')[1]);
+//             const ab = new ArrayBuffer(byteString.length);
+//             const ia = new Uint8Array(ab);
+
+//             for (let i = 0; i < byteString.length; i++) {
+//                 ia[i] = byteString.charCodeAt(i);
+//             }
+
+//             return new Blob([ab], { type: 'image/jpeg' });
+//         }
+//         return null;
+//     }
+
+//     get getImageUrl() {
+//         const image = this.dataURItoBlob();
+//         if (image) return URL.createObjectURL(image);
+//         return '';
+//     }
+
+
+// }
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
